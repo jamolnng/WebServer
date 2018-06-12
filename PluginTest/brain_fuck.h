50b20ed5ef9130f9fdbb28cc1102736bbfee263e
Jesse Laning <jesse@jlaning.com>
2018-06-11 22:47:17 -0400

    refactor

--- PluginTest/brain_fuck.h
+++ PluginTest/brain_fuck.h
@@ -2,36 +2,40 @@
 #ifdef max
 #undef max
 #endif
-#include <iostream>
 #include <limits>
 #include <stack>
 #include <string>
 #include <vector>
 
-template <typename ptr = unsigned short>
+template <typename _ptr = unsigned short>
 class BrainFuck {
  public:
-  BrainFuck(size_t _MAX_PROGRAM_SIZE = 4096, size_t _MAX_STACK_SIZE = 512)
-      : MAX_PROGRAM_SIZE(_MAX_PROGRAM_SIZE), MAX_STACK_SIZE(_MAX_STACK_SIZE) {}
+  using ptr = _ptr;
+
+  BrainFuck(size_t maxProgramSize = 4096, size_t maxStackSize = 512)
+      : maxProgramSize(maxProgramSize), maxStackSize(maxStackSize) {}
+
   void compile(const std::string& str) {
     program.clear();
     ptr pc = 0;
     ptr jump;
     std::stack<ptr> stack;
     for (char c : str) {
-      if (pc == MAX_PROGRAM_SIZE)
-        throw std::exception("max program size reached");
+      if (pc == maxProgramSize)
+        throw std::exception("Program excedes max program size.");
       switch (c) {
         case ']':
-          if (stack.size() == 0) throw std::exception("stack not populated");
+          if (stack.size() == 0)
+            throw std::exception(
+                "Stack error. Are you missing an opening '['?");
           jump = stack.top();
           stack.pop();
           program.push_back(Instruction(c, jump));
           program[jump].operand = pc;
           break;
         case '[':
-          if (stack.size() == MAX_STACK_SIZE)
-            throw std::exception("max stack size reached");
+          if (stack.size() == maxStackSize)
+            throw std::exception("Max stack size exceded.");
           stack.push(pc);
         case '>':
         case '<':
@@ -48,18 +52,18 @@ class BrainFuck {
       pc++;
     }
     if (!stack.empty())
-      throw std::exception("stack not closed (are you missing a closing ']')");
-    if (pc == MAX_PROGRAM_SIZE)
-      throw std::exception("program excedes max program size");
+      throw std::exception("Stack error. Are you missing a closing ']'?");
+    if (pc == maxProgramSize)
+      throw std::exception("Program excedes max program size.");
     program.push_back(Instruction('\0'));
   }
 
   template <class _in, class _out>
   void exec(_in& in, _out& out) {
-    std::vector<ptr> data(MAX_DATA_SIZE, ptr());
+    std::vector<ptr> data(maxDataSize, ptr());
     ptr pc = 0;
     ptr p = 0;
-    while (program[pc].opt != '\0' && p < MAX_DATA_SIZE) {
+    while (program[pc].opt != '\0' && p < maxDataSize) {
       switch (program[pc].opt) {
         case '>':
           p++;
@@ -90,7 +94,7 @@ class BrainFuck {
       }
       pc++;
     }
-    if (p == MAX_DATA_SIZE)
+    if (p == maxDataSize)
       throw std::exception("data limit exceeded during execution");
   }
 
@@ -103,7 +107,7 @@ class BrainFuck {
     ptr operand{};
   };
   std::vector<Instruction> program;
-  size_t MAX_PROGRAM_SIZE;
-  size_t MAX_STACK_SIZE;
-  size_t MAX_DATA_SIZE{std::numeric_limits<ptr>::max()};
+  size_t maxProgramSize;
+  size_t maxStackSize;
+  size_t maxDataSize{std::numeric_limits<ptr>::max()};
 };
